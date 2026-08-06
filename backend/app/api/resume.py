2e1ece649790b17682fa38e4f716f97d0f2d284f
Shaurya3030 <shauryapandey3030@gmail.com>
2026-08-06 12:22:30 +0530

    Implement PDF text extraction using PyMuPDF

--- backend/app/api/resume.py
+++ backend/app/api/resume.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, UploadFile, File
+from app.utils.pdf_reader import extract_text_from_pdf
 
 router = APIRouter()
 
 @router.post("/upload-resume")
 async def upload_resume(file: UploadFile = File(...)):
+
+    text = extract_text_from_pdf(file)
+
     return {
         "status": "success",
         "filename": file.filename,
-        "content_type": file.content_type
+        "text": text
     }
